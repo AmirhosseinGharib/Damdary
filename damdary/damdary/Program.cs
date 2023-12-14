6d7e0e974a76f3430e9bb893872197993e0a17ef
AmirhosseinGharib <149072424+AmirhosseinGharib@users.noreply.github.com>
2023-12-14 21:09:06 +0330

    Add files via upload

---
+++ damdary/damdary/Program.cs
@@ -0,0 +1,18 @@
+﻿List<Animal> animals = new List<Animal>()
+{
+    new Cow () { IsMale = true , Weight=250 , BirthDate=new DateTime(2022,10,05)},
+   new Sheep() { IsMale = false, Weight=100,  BirthDate = new DateTime(2021, 06, 07) },
+   new Sheep() { IsMale = false, Weight=80,   BirthDate = new DateTime(2021, 06, 07) }
+};
+
+
+foreach (Animal animal in animals)
+{
+    Console.WriteLine(animal.Life());
+}
+
+public class HealthParameter
+{
+
+}
+
